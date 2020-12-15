b5f8aeeeb6c5414d3c063ee20d70f84447f94ca3
iquidus <luke@iquidus.io>
2020-12-15 17:44:23 +0100

    account: workers tab, format header values

--- pages/account/_id.vue
+++ pages/account/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <v-col cols="12">
-    <v-row no-gutters>
+    <v-row no-gutters style="border-bottom: 1px solid #2e2e2e;">
       <v-col cols="12" md="4" sm="4" xs="12">
         <v-list dense>
           <v-list-item class="border-right">
@@ -44,7 +44,7 @@
             </v-list-item-avatar>
             <v-list-item-content>
               <v-list-item-title>LAST SHARE</v-list-item-title>
-              <v-list-item-subtitle>{{ data.stats.lastShare }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ formatTimeSince(data.stats.lastShare) }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item class="border-right">
@@ -99,11 +99,59 @@
         </v-list>
       </v-col>
     </v-row>
+    <v-tabs grow v-model="tab">
+      <v-tab>
+        Workers <v-chip label x-small class="ml-2">{{ data.workersTotal }}</v-chip>
+      </v-tab>
+      <v-tab>
+        Payouts
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item >
+        <v-simple-table fixed-header>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">ID</th>
+                <th class="text-left">Hashrate (rough, short average)</th>
+                <th class="text-left">Hashrate (accurate, long average)</th>
+                <th class="text-left">Last Share</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in data.workers" :key="index">
+                <td>{{ index }}</td>
+                <td>{{ formatHashrate(item.hr, true) }}</td>
+                <td>{{ formatHashrate(item.hr2, true) }}</td>
+                <td>{{ formatTimeSince(item.lastBeat) }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+        <v-alert type="info">
+          Your average hashrate will be smoothly adjusted until you have shares to fullfill estimation window.
+There are two windows, long and short, first is equal to about 30 minutes and long window is usually equal to 3 hours.
+Dead (sick) workers will be highlighted in a table of workers if they didn't submit a share for 1/2 of short window, so you can perform maintenance of your rigs. 
+        </v-alert>
+        <v-alert type="info">
+          Your bulk stats JSON API URL:
+          <a
+            :href="url + 'accounts/0xda904bc07fd95e39661941b3f6daded1b8a38c71'"
+            target="_blank"
+            style="color:#fff;"
+          >
+            {{ url + "accounts/0xda904bc07fd95e39661941b3f6daded1b8a38c71" }}
+          </a>
+        </v-alert>
+      </v-tab-item>
+    </v-tabs-items>
   </v-col>
 </template>
 
 <script>
 import axios from 'axios'
+import { formatDistance, formatDistanceToNow } from 'date-fns'
 
 export default {
   async asyncData({ params }) {
@@ -114,6 +162,7 @@ export default {
     return {
       url: 'http://127.0.0.1:8080/api/',
       errors: [],
+      tab: null,
       data: {
         workers: {},
         workersOffline: 0,
@@ -131,16 +180,16 @@ export default {
           lastShare: 0
         },
       },
-      nf: new Intl.NumberFormat("en", {}),
-      rtf: new Intl.RelativeTimeFormat("en", {
-        localeMatcher: "lookup", // other values: "lookup"
-        numeric: "always", // other values: "auto"
-        style: "short", // other values: "short" or "narrow"
-      })
+      nf: new Intl.NumberFormat("en", {})
     }
   },
   created() {
     this.fetch()
+  },
+  computed: {
+    now() {
+      return this.$store.state.now
+    },
   },
   methods: {
     async fetch() {
@@ -154,6 +203,9 @@ export default {
         console.log(error)
         this.errors.push(error)
       }
+    },
+    formatTimeSince(time) {
+      return formatDistance(new Date(time*1000), this.now, { addSuffix: true, includeSeconds: true })
     },
     formatHashrate(bytes, showHash) {
       const sizes = ['', 'K', 'M', 'G', 'T']
@@ -174,7 +226,11 @@ export default {
       let ether = shannon/1000000000
       // format nicely without losing precision
       let split = ether.toString().split('.')
-      return this.nf.format(split[0]) + '.' + split[1]
+      if (split.length > 1) {
+        return this.nf.format(split[0]) + '.' + split[1]
+      } else {
+        return this.nf.format(ether)
+      }
     }
   }
 }
