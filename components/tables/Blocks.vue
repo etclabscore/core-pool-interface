4f73d6c3a85d0fac43b767ff2a4f29fa23172676
iquidus <luke@iquidus.io>
2020-12-18 22:26:24 +0100

    Blocks: add search to table

--- components/tables/Blocks.vue
+++ components/tables/Blocks.vue
@@ -1,31 +1,44 @@
 <template>
-  <v-data-table
-    dense
-    :headers="headers"
-    :items="blocks"
-    :footer-props="{itemsPerPageText: 'Blocks per page', itemsPerPageOptions: [25, 50, 100]}"
-    :items-per-page="25"
-    no-data-text="No matured blocks"
-  >
-    <template v-slot:item.height="{ item }">
-      {{ nf.format(item.height) }}
-    </template>
-    <template v-slot:item.shares="{ item }">
-      {{ nf.format(((item.shares/item.difficulty)*100).toFixed(0)) }}%
-    </template>
-    <template v-slot:item.uncle="{ item }"> 
-      <v-chip label small :color="formatBlockType(item).color">{{ formatBlockType(item).text }}</v-chip>
-    </template>
-    <template v-slot:item.timestamp="{ item }">
-      {{ dtf.format(item.timestamp*1000) }}
-    </template>
-    <template v-slot:item.hash="{ item }">
-      {{ formatAccountHash(item.hash) }}
-    </template>
-    <template v-slot:item.reward="{ item }">
-      {{ formatReward(item.reward).toFixed(6) }}
-    </template>
-  </v-data-table>
+  <v-card flat tile>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        outlined
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="blocks"
+      :footer-props="{itemsPerPageText: 'Blocks per page', itemsPerPageOptions: [25, 50, 100]}"
+      :items-per-page="25"
+      :search="search"
+      no-data-text="No matured blocks"
+    >
+      <template v-slot:item.height="{ item }">
+        {{ nf.format(item.height) }}
+      </template>
+      <template v-slot:item.shares="{ item }">
+        {{ nf.format(((item.shares/item.difficulty)*100).toFixed(0)) }}%
+      </template>
+      <template v-slot:item.uncle="{ item }"> 
+        <v-chip label small :color="formatBlockType(item).color">{{ formatBlockType(item).text }}</v-chip>
+      </template>
+      <template v-slot:item.timestamp="{ item }">
+        {{ dtf.format(item.timestamp*1000) }}
+      </template>
+      <template v-slot:item.hash="{ item }">
+        {{ formatAccountHash(item.hash) }}
+      </template>
+      <template v-slot:item.reward="{ item }">
+        {{ formatReward(item.reward).toFixed(6) }}
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
@@ -46,6 +59,7 @@ export default {
   },
   data () {
     return {
+      search: null,
       headers: [
         {
           text: 'Height',
