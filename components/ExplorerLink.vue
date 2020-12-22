a359663dff8ad184a689e3aa4fdc6ac1b7f60b43
iquidus <luke@iquidus.io>
2020-12-22 19:59:18 +0100

    ExplorerLink: implement EIP-3091 (explorer.type: expedition, blockscout, spectrum, etherscan, etherchain)

--- components/ExplorerLink.vue
+++ components/ExplorerLink.vue
@@ -18,10 +18,16 @@ export default {
         return "tx"
       }
     },
-    clip: { // shorten inner url to (0x[clip]...[clip]) characters
+    clip: { // shorten inner url to (0x[clip][separator][clip]) characters. 0 = dont clip.
       type: Number,
       default() {
         return 0
+      }
+    },
+    separator: {
+      type: String,
+      default() {
+        return '...'
       }
     },
     config: { // config
@@ -33,19 +39,66 @@ export default {
   },
   methods: {
     formatUrl() {
+      // see: https://github.com/ethereum/EIPs/pull/3091
+      // explorer.type
+      // expedition, blockscout, etherscan, etherchain, spectrum
+      // note, most of these are fairly similar however keeping all as options incase of future deviations.
       let url = this.config.explorer.url
+      let type = this.config.explorer.type
       let network = this.config.network
-      if (this.config.explorer.type === "expedition") {
-        if (this.linkType === "block") {
-          url = url + '/block/' + this.hash
-        } else {
-          url = url + '/tx/' + this.hash
-        }
-        if (network === 'classic') {
-          network = 'mainnet'
-        }
-        return url + '?network=' + network
+      let symbol = this.config.symbol.toLowerCase()
+      let append = '/'
+      
+      // handle link type deviations
+      switch(this.linkType) {
+        case 'block':
+          if (type === 'blockscout') {
+            append = append + 'blocks/'
+          } else { // etherscan, etherchain or expedition, spectrum
+            append = append + 'block/'
+          }
+          break
+        case 'account':
+          if (type === 'etherchain') {
+            append = append + 'account/'
+          } else { // etherscan, blockscout, expedition, spectrum
+            append = append + 'address/'
+          }
+          break
+        case 'token':
+          if (type === 'blockscout') {
+            append = append + 'tokens/'
+          } else { // etherscan, etherchain, expedition, spectrum
+            append = append + 'token/'
+          }
+          break
+        case 'tx':
+          append = append + 'tx/' // yayyy conformity
+          break
+        default:
+          // o.O something very odd has occured O.o
+          // check your link-type argument, it should be:
+          // block, account, token, or tx
       }
+
+      // handle network deviations
+      switch(type) {
+        case 'expedition':
+          if (network === 'classic') {
+            network = 'mainnet'
+          }
+          append = append + this.hash + '?network=' + network
+          break
+        case 'blockscout':
+          if (network === 'classic') {
+            network = 'mainnet'
+          }
+          append = '/' + symbol + '/' + network + append + this.hash
+          break
+        default: 
+          append = append + this.hash
+      }
+      return url + append
     },
     formatHash(hash, len) {
       if (hash === '0x0' || !hash) {
@@ -54,7 +107,7 @@ export default {
       if (len > 0) {
         const start = hash.substring(0,len+2)
         const end = hash.substring(hash.length-len)
-        return start + '...' + end
+        return start + this.separator + end
       }
       return hash
     },
