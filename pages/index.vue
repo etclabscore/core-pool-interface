ea588cd572aacb4e54d7bc2381af7eb7cd2f0b00
iquidus <luke@iquidus.io>
2020-12-30 14:42:59 +0100

    index: fix core-pool url (html, not md), add network info

--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,44 @@
   <v-row justify="center" align="center" no-gutters class="pa-0">
     <v-col cols="12" class="pa-0">
       <v-card flat tile class="mb-0">
-        <v-img height="200" :src="require('~/static/banner.jpg')" gradient="to top right, rgba(0,0,0,.8), rgba(255,255,201,.33)" class="white--text align-end">
+        <v-img height="200" :src="require('~/static/banner.jpg')" gradient="to top right, rgba(0,0,0,.9), rgba(255,255,201,.33)" class="white--text align-end">
           <v-card-title>
             <v-list style="background-color:rgba(0,0,0,0);">
-              <v-list-item style="background-color:rgba(0,0,0,0);color:#fff;">
+              <v-list-item style="background-color:rgba(0,0,0,0);">
                 <v-list-item-avatar>
                   <img :src="require('~/static/' + config.logo)"/>
                 </v-list-item-avatar>
                 <v-list-item-content>
-                  <v-list-item-title>{{ config.title }}</v-list-item-title>
-                  <v-list-item-subtitle>{{ config.description }}</v-list-item-subtitle>
+                  <v-list-item-title class="white--text">{{ config.title }}</v-list-item-title>
+                  <v-list-item-subtitle class="white--text">{{ config.description }}</v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
             </v-list>
+            <v-spacer />
+            <v-list style="background-color:rgba(0,0,0,0);">
+                <v-list-item style="background-color:rgba(0,0,0,0);">
+                  <v-list-item-avatar>
+                    <img :src="require('~/static/' + config.network.icon)"/>
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title>{{ config.network.title }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ config.network.algo }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
           </v-card-title>
         </v-img>
         <v-alert v-if="network.testnet" outlined text dismissible tile type="warning" class="w-100 mb-0">
           This pool is configured for the {{ network.title }}. The {{ network.symbol }} rewarded is testnet {{ network.symbol }}.
         </v-alert>
-        <v-row no-gutters>
-          <v-col cols="12" xs="12">
-            <v-card-text>
-              <ul>
-                <li>Min. payout threshold: {{ config.payoutThreshold }} {{ config.network.symbol }}.</li>
-                <li>{{ config.mode }} stable and profitable pool with regular payouts.</li>
-                <li>Powered by [core-pool](https://github.com/etclabscore/core-pool).</li>
-                <li>GetWork & Stratum supported.</li>
-              </ul>
-            </v-card-text>
-          </v-col>
-        </v-row>
+        <v-card-text class="py-1">
+          <ul>
+            <li>Min. payout threshold: {{ config.payoutThreshold }} {{ config.network.symbol }}.</li>
+            <li>{{ config.mode }} stable and profitable pool with regular payouts.</li>
+            <li>Powered by <a href="https://github.com/etclabscore/core-pool" target="_blank">core-pool</a>.</li>
+            <li>GetWork & Stratum supported.</li>
+          </ul>
+        </v-card-text>
       </v-card>
       <v-card flat tile>
         <v-card-title>
